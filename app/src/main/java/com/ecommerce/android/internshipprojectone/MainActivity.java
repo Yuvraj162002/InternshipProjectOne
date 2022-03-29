3393e2cb67a5acd358af2ab0cabb3836422d87cb
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2022-03-30 02:17:46 +0530

    30March

--- app/src/main/java/com/ecommerce/android/internshipprojectone/MainActivity.java
+++ app/src/main/java/com/ecommerce/android/internshipprojectone/MainActivity.java
@@ -2,13 +2,59 @@ package com.ecommerce.android.internshipprojectone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.classone);
+        button1 = (Button) findViewById(R.id.classtwo);
+        button2 = (Button) findViewById(R.id.classthree);
+        button3 = (Button) findViewById(R.id.classfour);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , SubjectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , SubjectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , SubjectActivity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , SubjectActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
+
